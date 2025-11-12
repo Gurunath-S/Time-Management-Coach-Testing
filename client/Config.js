070b28a97fe07a5019533da1bf406396bd62c578
Guru <139735814+Gurunath-S@users.noreply.github.com>
2025-11-12 18:39:53 +0530

    Uncomment BACKEND_URL definition in Config.js

--- client/Config.js
+++ client/Config.js
@@ -1,8 +1,9 @@
 // const BACKEND_URL = "https://time-management-coach-testing.onrender.com";
 
-const BACKEND_URL = " https://time-management-coach-testing.onrender.com";
+const BACKEND_URL = "https://time-management-coach-testing.onrender.com";
 export default BACKEND_URL;
 
 // https://time-management-coach-testing.onrender.com 
+
 
 
